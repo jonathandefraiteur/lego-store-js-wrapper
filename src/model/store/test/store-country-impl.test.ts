8d1fd044a1ee9debf7b1015d5711313cb87b22fc
Jonathan <jonathan.defraiteur@gmail.com>
2021-06-10 01:04:40 +0200

    test: improve store-country-impl tests

--- src/model/store/test/store-country-impl.test.ts
+++ src/model/store/test/store-country-impl.test.ts
@@ -3,57 +3,61 @@ import { CountryCode } from '../country-code';
 import { LanguageCode } from '../language-code';
 import { WorldRegion } from '../world-region';
 
-describe(StoreCountryImpl.name, () => {
-  test.each([
-    [CountryCode.CN, [LanguageCode.ZH], 'zh-cn'],
-    [
-      CountryCode.BE,
-      [LanguageCode.FR, LanguageCode.NL, LanguageCode.EN],
-      'fr-be'
-    ],
-    [CountryCode.RU, [], 'en-ru']
-  ])(
-    '.getStorePath()',
-    (country: CountryCode, languages: LanguageCode[], expected: string) => {
-      const store = new StoreCountryImpl(
-        WorldRegion.NorthAmerica,
-        country,
-        languages
-      );
-      expect(store.getStorePath()).toBe(expected);
-    }
-  );
+describe(`.getStorePath(language?)`, () => {
+  describe('new (region, country, languages).getStorePath() - expected Lang-Country', () => {
+    test.each([
+      [CountryCode.CN, [LanguageCode.ZH], 'zh-cn'],
+      [
+        CountryCode.BE,
+        [LanguageCode.FR, LanguageCode.NL, LanguageCode.EN],
+        'fr-be'
+      ],
+      [CountryCode.RU, [], 'en-ru']
+    ])(
+      'new (Europe, %p, %p).getStorePath() = %p',
+      (country: CountryCode, languages: LanguageCode[], expected: string) => {
+        const store = new StoreCountryImpl(
+          WorldRegion.Europe,
+          country,
+          languages
+        );
+        expect(store.getStorePath()).toBe(expected);
+      }
+    );
+  });
 
-  test.each([
-    [
-      CountryCode.BE,
-      [LanguageCode.FR, LanguageCode.NL, LanguageCode.EN],
-      LanguageCode.NL,
-      'nl-be'
-    ],
-    [
-      CountryCode.FI,
-      [LanguageCode.EN, LanguageCode.FI],
-      LanguageCode.FI,
-      'fi-fi'
-    ],
-    [CountryCode.CN, [LanguageCode.ZH], LanguageCode.ZH, 'zh-cn'],
-    [CountryCode.CN, [LanguageCode.ZH], LanguageCode.EN, 'zh-cn'],
-    [CountryCode.RU, [], LanguageCode.RU, 'en-ru']
-  ])(
-    '.getStorePath(language)',
-    (
-      country: CountryCode,
-      languages: LanguageCode[],
-      language: LanguageCode,
-      expected: string
-    ) => {
-      const store = new StoreCountryImpl(
-        WorldRegion.NorthAmerica,
-        country,
-        languages
-      );
-      expect(store.getStorePath(language)).toBe(expected);
-    }
-  );
+  describe('new (region, country, languages).getStorePath(language) - expected Lang-Country', () => {
+    test.each([
+      [
+        CountryCode.BE,
+        [LanguageCode.FR, LanguageCode.NL, LanguageCode.EN],
+        LanguageCode.NL,
+        'nl-be'
+      ],
+      [
+        CountryCode.FI,
+        [LanguageCode.EN, LanguageCode.FI],
+        LanguageCode.FI,
+        'fi-fi'
+      ],
+      [CountryCode.CN, [LanguageCode.ZH], LanguageCode.ZH, 'zh-cn'],
+      [CountryCode.CN, [LanguageCode.ZH], LanguageCode.EN, 'zh-cn'],
+      [CountryCode.RU, [], LanguageCode.RU, 'en-ru']
+    ])(
+      'new (Europe, %p, %p).getStorePath(%p) = %p',
+      (
+        country: CountryCode,
+        languages: LanguageCode[],
+        language: LanguageCode,
+        expected: string
+      ) => {
+        const store = new StoreCountryImpl(
+          WorldRegion.Europe,
+          country,
+          languages
+        );
+        expect(store.getStorePath(language)).toBe(expected);
+      }
+    );
+  });
 });
